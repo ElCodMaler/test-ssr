b9ab2e53aa19279c13108930a7f8b72ccb4287c5
ElCodMaler <gilbertojeda30@gmail.com>
2024-05-13 21:47:06 -0400

    cambio de url 2

--- fisiotFitnes/vite.config.js
+++ fisiotFitnes/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://elcodmaler.github.io/test-ssr/fisiotFitnes/"
+  base: "https://elcodmaler.github.io/test-ssr/"
 })
